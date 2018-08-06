896e3acea489be9f2e7281f33c5bbd9a0d129c6c
dev-esoftplay <38025170+dev-esoftplay@users.noreply.github.com>
2018-08-07 00:07:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from '../react'
-import { Image, PixelRatio, ImageEditor } from '../../node_modules/react-native/Libraries/react-native/react-native-implementation.js';
+import { Image, PixelRatio, ImageEditor, StyleSheet, View } from '../../node_modules/react-native/Libraries/react-native/react-native-implementation.js';
 import { configConsole } from '../../config'
+
 
 export default class FastImage extends React.PureComponent {
 
@@ -8,44 +9,90 @@ export default class FastImage extends React.PureComponent {
     image: null,
   }
 
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.source.hasOwnProperty('uri') && prevProps.source.uri !== this.props.source.uri) {
+      configConsole('1st')
+      this.setState({ image: null })
+      this.processImage(this.props.source)
+    } else if (!prevProps.source.hasOwnProperty('uri') && prevProps.source !== this.props.source) {
+      configConsole('2st')
+      this.setState({ image: null })
+      this.processImage(this.props.source)
+    }
+  };
+
   compress(uri, w, h, callback) {
-    var quality = this.props.quality || 1
-    var destWidth = this.props.width
-    var destHeight = this.props.height
-    if (this.props.resizeMode == 'contain') {
-      if (destWidth > destHeight) {
-        destHeight = h / w * destWidth
-      } else {
-        destWidth = w / h * destHeight
+    if (uri && w && h && callback) {
+      var quality = this.props.quality || 1
+      var { width, height, resizeMode } = StyleSheet.flatten(this.props.style)
+      var destHeight = height
+      var destWidth = width
+      var destResizeMode = resizeMode
+      if (!width) {
+        destWidth = this.props.width
+      }
+      if (!height) {
+        destHeight = this.props.height
+      }
+      if (!resizeMode) {
+        destResizeMode = this.props.resizeMode
+      }
+      if (destResizeMode == 'contain') {
+        if (destWidth > destHeight) {
+          destHeight = h / w * destWidth
+        } else {
+          destWidth = w / h * destHeight
+        }
+      }
+      ImageEditor.cropImage(uri,
+        {
+          offset: { x: 0, y: 0 },
+          size: {
+            width: w,
+            height: h
+          },
+          displaySize: {
+            width: PixelRatio.getPixelSizeForLayoutSize(destWidth * quality),
+            height: PixelRatio.getPixelSizeForLayoutSize(destHeight * quality)
+          },
+          resizeMode: 'contain',
+        },
+        (uri) => callback({ uri: uri }),
+        (msg) => configConsole(msg)
+      )
+    }
+  }
+
+  processImage(source) {
+    if (source.hasOwnProperty('uri')) {
+      Image.getSize(source.uri, (w, h) => {
+        this.compress(source.uri, w, h, (res) => {
+          this.setState({ image: res })
+        })
+      }, (error) => {
+
+      })
+    } else {
+      var image = Image.resolveAssetSource(source)
+      if (image) {
+        this.compress(image.uri, image.width, image.height, (res) => {
+          this.setState({ image: res })
+        })
       }
     }
-    ImageEditor.cropImage(uri,
-      {
-        offset: { x: 0, y: 0 },
-        size: {
-          width: w,
-          height: h
-        },
-        displaySize: {
-          width: PixelRatio.getPixelSizeForLayoutSize(destWidth * quality),
-          height: PixelRatio.getPixelSizeForLayoutSize(destHeight * quality)
-        },
-        resizeMode: 'contain',
-      },
-      (uri) => callback({ uri: uri }),
-      (msg) => configConsole(msg)
-    )
   }
 
   componentDidMount = () => {
-    Image.getSize(this.props.uri, (w, h) => {
-      this.compress(this.props.uri, w, h, (res) => {
-        this.setState({ image: res })
-      })
-    })
+    this.processImage(this.props.source)
   };
 
   render() {
-    return <Image style={[{ ...this.props.style }, { width: this.props.width, margin: 1, height: this.props.height, resizeMode: this.props.resizeMode || 'cover' }]} source={this.state.image} />
+    var style = StyleSheet.flatten(this.props.style)
+    if (!this.state.image) {
+      var s = Object.assign({}, style)
+      delete s['resizeMode']
+      return <View style={[{ backgroundColor: '#f5f5f5' }, { ...s }]} />
+    }
+    return <Image style={[{ resizeMode: 'contain' }, { ...style }]} source={this.state.image} />
   }
 }
